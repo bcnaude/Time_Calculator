57057fb17f4dff3028b9cdc6de02e2fde3265d93
Chris Naude <46300266+bcnaude@users.noreply.github.com>
2023-02-01 08:08:20 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+A function that takes in two required parameters and one optional parameter: a start time in the 12-hour clock format (ending in AM or PM), a duration time that indicates the number of hours and minutes, (optional) a starting day of the week, case insensitive. The function adds the duration time to the start time and return the result.
---
+++ time_calculator.py
@@ -0,0 +1,85 @@
+def add_time(start_time, duration_time, weekday=""):
+
+    split_time = start_time.replace(":", " ").split()
+    split_duration = duration_time.split(":")
+
+    # Calculate total hours and minutes
+    total_hours = int(split_time[0]) + int(split_duration[0])
+    total_minutes = int(split_time[1]) + int(split_duration[1])
+    total_days = 0
+
+    # If minutes are 60 or more convert them to hours
+    if total_minutes > 59:
+        total_hours += total_minutes // 60
+        total_minutes = total_minutes % 60
+
+    # If hours are 24 or more convert them to days
+    if total_hours > 23:
+        total_days = total_hours // 24
+        total_hours = total_hours % 24
+
+    # Calculate if suffix should be AM or PM
+    time_suffix = ""
+    if split_time[2] == "AM":
+        if total_hours % 24 < 12:
+            time_suffix = "AM"
+        else:
+            time_suffix = "PM"
+    else:
+        if total_hours % 24 < 12:
+            time_suffix = "PM"
+        else:
+            time_suffix = "AM"
+            total_days += 1
+
+    # Convert hours from 24 hour format to 12 hour format
+    if total_hours > 12:
+        total_hours = total_hours - 12
+
+    # Build hour string
+    total_hours = str(total_hours)
+
+    # Build minute string
+    total_minutes = str(total_minutes).zfill(2)
+
+    # Build day string
+    total_days = str(total_days)
+
+    # Build string after the time
+    after_the_time = ""
+    if int(total_days) == 1:
+        after_the_time = " (next day)"
+    elif int(total_days) > 1:
+        after_the_time = f" ({total_days} days later)"
+
+    # Build return string
+    if weekday:
+        # Find value of weekday in weekdays and calculate value for the new day of week
+        weekday = weekday.lower()
+        weekdays = {
+            "monday": 0,
+            "tuesday": 1,
+            "wednesday": 2,
+            "thursday": 3,
+            "friday": 4,
+            "saturday": 5,
+            "sunday": 6,
+        }
+
+        if weekday in weekdays:
+            new_day_value = (weekdays[weekday] + int(total_days)) % 7
+        # Use the new value to find the new weekday key
+        weekday = (
+            list(weekdays.keys())[list(weekdays.values()).index(new_day_value)]
+        ).title()
+
+        new_time = (
+            f"{total_hours}:{total_minutes} {time_suffix}, {weekday}{after_the_time}"
+        )
+    else:
+        new_time = f"{total_hours}:{total_minutes} {time_suffix}{after_the_time}"
+
+    return new_time
+
+
+# print(add_time("11:43 PM", "24:20", "tuesday"))
